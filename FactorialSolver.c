517a7add5c3d1c49c5d08f8f8b22b222eaa53db9
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2024-01-28 22:19:26 +0530

    Create FactorialSolver.c

---
+++ FactorialSolver.c
@@ -0,0 +1,18 @@
+// A C program to calculate the factorial of a number, with options for both recursive and non-recursive implementations.
+
+#include<stdio.h>
+long int factorial(int n);
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, factorial(n));
+    return 0;
+}
+
+long int factorial(int n) {
+    if (n>=1)
+        return n*factorial(n-1);
+    else
+        return 1;
+}
