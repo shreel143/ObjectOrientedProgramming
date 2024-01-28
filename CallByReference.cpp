7c44fc7c2642a350508af9569624008d17ab88fb
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2024-01-28 22:59:04 +0530

    Create CallByReference.cpp

---
+++ CallByReference.cpp
@@ -0,0 +1,31 @@
+// A program to swap two numbers using the call by reference method.
+
+#include <iostream>  
+using namespace std;  
+  
+void swap_by_ref(int &x,int &y){  
+ int temp;  
+ temp=x;  
+ x=y;  
+ y=temp;  
+  
+ cout<<"\nValues of numbers after swapping in swap function:"<<endl;  
+ cout<<"First Number:"<<x<<endl;  
+ cout<<"Second Number:"<<y<<endl;  
+}  
+  
+int main()  
+{  
+ int a,b;  
+  
+ cout<<"Enter the value of first number :"<<endl;  
+ cin>>a;  
+  
+ cout<<"Enter the value of second number :"<<endl;  
+ cin>>b;  
+  
+ cout<<"\nValues of numbers before swapping :"<<endl;  
+ cout<<"First Number:"<<a<<endl;  
+ cout<<"Second Number:"<<b<<endl;  
+  
+ swap_by_ref(a,b); 
